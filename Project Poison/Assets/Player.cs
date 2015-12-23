36b1577b38376098512cf474f292cdb13434669c
Erik Lindsay (egl602) <egl602@mail.usask.ca>
2015-12-22 19:46:45 -0600

    Stuff
    Happening

---
+++ Project Poison/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    float attackStat;
+    float defense;
+    int health;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
