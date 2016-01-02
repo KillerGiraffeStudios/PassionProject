bf2d142f268373e110c62d762b4e69962fb09509
Erik Lindsay (egl602) <egl602@mail.usask.ca>
2016-01-01 20:07:17 -0600

    Removed Duplicates, adding small code to player
    2 scripts had duplicates, causing compile errors, removing them. Also
    slight changes to player script

--- Project Poison/Assets/Scripts/Player.cs
+++ Project Poison/Assets/Scripts/Player.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
 
 public class Player : MonoBehaviour {
 
     float attack;
     float defense;
+    int health;
+    int energy;
+    float critChance;
+    float critMultiplier;
+    System.Random rand;
+
 
 	// Use this for initialization
 	void Start () {
-	
+        rand = new System.Random();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
 }
