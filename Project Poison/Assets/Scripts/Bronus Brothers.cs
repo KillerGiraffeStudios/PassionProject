06e90f864814acb542c157385b60d560429f4ff2
Erik Lindsay (egl602) <egl602@mail.usask.ca>
2016-01-15 13:44:06 -0600

    Bronus Bros
    made boss stuff

---
+++ Project Poison/Assets/Scripts/Bronus Brothers.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BronusBrothers : MonoBehaviour {
+
+
+    int attack;
+    int defense;
+    int dodge;
+    int hit;
+    int health;
+    int energy;
+    int critChance;
+    float critMultiplier;
+    System.Random rand;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rand = new System.Random();
+        health = 80;
+        hit = 90;
+        dodge = 10;
+        critChance = 10;
+        attack = 4;
+        critMultiplier = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    bool Hit(int dodge)
+    {
+        int random = rand.Next(0, hit);
+        if (random > dodge)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool Dodge(int hit)
+    {
+        int random = rand.Next(0, hit);
+        if (random > dodge)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void DamageRecieved(int enemyDamage)
+    {
+        health = enemyDamage - defense;
+    }
+
+    int Dealdamage()
+    {
+        int damage;
+        bool crit = Crit();
+        if (crit)
+        {
+            damage = (int)(attack * critMultiplier);
+        }
+        else
+        {
+            damage = attack;
+        }
+        return damage;
+    }
+
+    bool Crit()
+    {
+        int chance = rand.Next(0, 100);
+        if (chance <= critChance)
+        {
+            return true;
+        }
+        return false;
+    }
+}
