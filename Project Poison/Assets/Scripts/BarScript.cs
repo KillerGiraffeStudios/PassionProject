4423390e7ee27476fdc6d96f9d99837029e779a4
aj3x <ajrussell94@hotmail.com>
2016-01-15 16:48:00 -0600

    Update Bar script
    + testing in script

--- Project Poison/Assets/Scripts/BarScript.cs
+++ Project Poison/Assets/Scripts/BarScript.cs
@@ -6,12 +6,14 @@ public class BarScript : MonoBehaviour {
     RectTransform rect;
     Text barText;
     public int maxSize;
+    public int curSize;//DELETE
 	// Use this for initialization
 	void Start () {
         rect = gameObject.GetComponent<RectTransform>();
         //maxSize = 100;//getMaxSize from health/power script
         rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxSize);
         //setBackBar to +2 of max
+        curSize = maxSize;
         barText = GameObject.Find(gameObject.name + "Text").GetComponent<Text>();
         barText.text = maxSize + "/" + maxSize;
         GameObject.Find(gameObject.name+"Max").GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxSize + 2);
@@ -25,6 +27,15 @@ public class BarScript : MonoBehaviour {
         if (num < 0) num = 0;//uneeded later
         rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, num);
         barText.text = num + "/" + maxSize;
+    }
+
+    /// <summary>
+    /// ****DELETE****
+    /// </summary>
+    /// <param name="num"></param>
+    public void takeDam(int num) {
+        curSize -= num;
+        setBar(curSize);
     }
 
 	// Update is called once per frame
