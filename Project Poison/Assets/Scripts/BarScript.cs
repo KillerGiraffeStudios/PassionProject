025f3a0a8b69ad634aaaf0c7be6dbe3ded3925ac
aj3x <ajrussell94@hotmail.com>
2016-01-15 10:07:28 -0600

    Update BarScript
    Attaches to any bar with a background bar
    Sets size based on set value(default 100)
    able to set bar length function

---
+++ Project Poison/Assets/Scripts/BarScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarScript : MonoBehaviour {
+    RectTransform rect;
+	// Use this for initialization
+	void Start () {
+        rect = gameObject.GetComponent<RectTransform>();
+        int num = 100;//getMaxSize from health/power script
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, num);
+        //setBackBar to +2 of max
+        GameObject.Find("Max"+gameObject.name).GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, num + 2);
+    }
+
+    /// <summary>
+    /// Sets bar to length of num
+    /// </summary>
+    /// <param name="num">value to set bar width to</param>
+    public void setBar(int num) {
+        if (num < 0) num = 0;//uneeded later
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, num);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
