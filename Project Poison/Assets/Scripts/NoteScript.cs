f9c62499a705684082cd4baa5a659e1468262dd6
Darvin Zhang <ddz369@mail.usask.ca>
2016-01-18 23:13:25 -0600

    Removed test, added my scene, started rhythm game
    Created my scene and added small bits to try to form a working version
    of a beatmania style rhythm game. So far, the bars have been added.
    Movement and mapping are to follow.

---
+++ Project Poison/Assets/Scripts/NoteScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
