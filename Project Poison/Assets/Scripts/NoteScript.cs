2e7b448cbf3515d04a8e33371aeafaa5aae25604
Darvin Zhang <ddz369@mail.usask.ca>
2016-01-28 03:21:41 -0600

    Revert "Created my Scene, added notes"
    This reverts commit 6df186314db7ce4585d56e57945995a8c0854a23.

--- Project Poison/Assets/Scripts/NoteScript.cs
+++ Project Poison/Assets/Scripts/NoteScript.cs
@@ -5,14 +5,11 @@ public class NoteScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
+
 	}
 
-    //move the object downwards
-    public float movementSpeed = 10;
-
-    void Update()
-    {
-        transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
-    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
